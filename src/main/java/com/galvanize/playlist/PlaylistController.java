ea23e6d699fc476c0066a9ab6d2ba305f221ef8c
Prasad Nettem <rajprasad.nettem@gmail.com>
2021-04-10 21:15:49 +0530

    green on adding empty playlist

--- src/main/java/com/galvanize/playlist/PlaylistController.java
+++ src/main/java/com/galvanize/playlist/PlaylistController.java
@@ -1,16 +1,24 @@
 package com.galvanize.playlist;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlaylistController {
 
-
-
     @GetMapping("playlists")
     public String getPlaylists(){
         return "[{}]";
+    }
+
+    @PostMapping("playlists")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addPlaylists(){
+
+
     }
 }
