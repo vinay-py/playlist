6af20c91e5e95c274e2a40ccb7add831b7e55a21
Vinay <vinay.py@gmail.com>
2021-04-10 11:21:10 -0400

    GREEN - Get Playlists

---
+++ src/main/java/com/galvanize/playlist/PlaylistController.java
@@ -0,0 +1,16 @@
+package com.galvanize.playlist;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlaylistController {
+
+
+
+    @GetMapping("playlists")
+    public String getPlaylists(){
+        return "[]";
+    }
+}
